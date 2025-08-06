cd34ec5fd860ea893d6612b68cf88f2f2163db43
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-08-06 13:43:52 +0200

    Add AddLogging

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using ShopifyOrderAutomation.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddLogging();
 
 builder.Services.Configure<InPostConfiguration>(builder.Configuration.GetSection("InPostConfiguration"));
 builder.Services.Configure<ShopifyConfiguration>(builder.Configuration.GetSection("ShopifyConfiguration"));
