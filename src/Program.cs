2fb30ada4bb8996d3c0615692bf52d2eb244a707
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-08-06 14:15:08 +0200

    Add Swashbuckle.AspNetCore

--- src/Program.cs
+++ src/Program.cs
@@ -12,18 +12,19 @@ builder.Services.Configure<ShopifyConfiguration>(builder.Configuration.GetSectio
 builder.Services.AddHttpClient<IInPostService, InPostService>();
 builder.Services.AddHttpClient<IShopifyService, ShopifyService>();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
 app.Run();
